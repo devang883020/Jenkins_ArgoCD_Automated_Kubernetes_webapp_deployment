e72efce71decf8dee86502b198950220fc6b9c10
devang883020 <devangkubde632@gmail.com>
2025-12-18 17:12:04 +0530

    first push

---
+++ automated-k8s-cicd/app/main.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify
+import socket
+import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return f"""
+    <h1>🚀 Python Flask Web App</h1>
+    <p><b>Hostname:</b> {socket.gethostname()}</p>
+    <p><b>Time:</b> {datetime.datetime.now()}</p>
+    <p>Status: Running successfully</p>
+    """
+
+@app.route("/health")
+def health():
+    return jsonify(
+        status="UP",
+        service="python-webapp",
+        time=str(datetime.datetime.now())
+    )
+
+@app.route("/api/info")
+def info():
+    return jsonify(
+        app="Flask Docker Web App",
+        version="1.0",
+        author="Devang"
+    )
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
